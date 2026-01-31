0c0b47868b9768ec6294da059a9f9c832b578e51
liyunhui <liyunhui18367896477@gmail.com>
2026-01-31 09:34:59 +0800

    init: week1_project scaffold

---
+++ ENV.md
@@ -0,0 +1,5 @@
+# ENV
+- python3: Python 3.13.9
+- python3 path: /opt/anaconda3/bin/python3
+- date: 2026-01-31
+
---
+++ src/main.py
@@ -0,0 +1,6 @@
+import os
+
+print("Week1 Project: OK")
+print("Current working dir:", os.getcwd())
+print("Files here:", os.listdir("."))
+
